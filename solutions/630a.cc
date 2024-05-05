83c23f028d93825885d7f22d517a3ed5a9926d0a
p55d2k <231637w@student.hci.edu.sg>
2024-05-05 14:11:23 +0800

    630a

---
+++ solutions/630a.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;
+    cin >> n;
+    cout << 25 << endl;
+
+    return 0;
+}
